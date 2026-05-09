f36f958fa3906ba0a03c516b3bd37e2a82e4c2f3
Sivanandu-03 <sivanall03@gmail.com>
2026-05-09 21:02:38 +0530

    Fix allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
+  plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: true
+  }
 })
